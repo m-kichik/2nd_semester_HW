500ad1380b40c728b61443c7129bee18398adad6
m-kichik <kichik.mg@phystech.edu>
2021-02-15 14:07:48 +0300

    hw 2 task 3 v1

--- HW_2/2nd_lesson_task_3_VECTOR_TO_MULTIARRAY_RAW.cpp
+++ HW_2/2nd_lesson_task_3_VECTOR_TO_MULTIARRAY_RAW.cpp
@@ -26,7 +26,7 @@ void fill_multi_array(const Container & container, Forward_Iterator multi_array)
 	    for(auto i = 0u; i < container.size(); i++)
 	    {
             fill_multi_array< N - 1 >(container[i], std::next(multi_array, i) -> begin());
-        }
+	    }
     }
 	else
     {
