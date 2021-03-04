940313fbbfdf0dd84ed71856a9d8f9383271b4c6
m-kichik <kichik.mg@phystech.edu>
2021-03-05 01:23:45 +0300

    HW 5 t1 v1.1

--- HW_5/task1_CURRENCY_CONVERTER.cpp
+++ HW_5/task1_CURRENCY_CONVERTER.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <iomanip>
 
+#include <Windows.h>
+
 int main() {
+
     std::cin.imbue(std::locale("ru_RU.UTF8"));
     std::cout.imbue(std::locale("en_US.UTF8"));
     long double money;
     std::cin >> std::get_money(money);
     money /= 74.69;
-    std::cout << std::put_money(money);
+    std::cout << std::showbase << std::put_money(money, false);
 
     return 0;
 }
