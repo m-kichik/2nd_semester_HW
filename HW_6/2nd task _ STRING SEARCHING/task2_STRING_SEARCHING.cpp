927e66b35ba8bf56ae2b996d3e412db02a1331c6
m-kichik <kichik.mg@phystech.edu>
2021-03-11 19:55:55 +0300

    HW 6 t2 v1

---
+++ HW_6/2nd task _ STRING SEARCHING/task2_STRING_SEARCHING.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+
+std::size_t getStringSize (std::ifstream& file) {
+    file.seekg(0);
+    char buf = 0;
+    auto size = 0u;
+        for (; buf != '\n' & buf != '\r'; ++size)
+            file.get(buf);
+        for (; buf == '\n' || buf == '\r'; ++size)
+            file.get(buf);
+    return size;
+}
+
+int main() {
+    std::ifstream file("../HW_6/2nd task _ STRING SEARCHING/file_with_random_strings.txt");
+
+    auto sizeOfStrings = getStringSize(file);
+
+    std::cout << "Enter line number: " << std::endl;
+    auto lineNumber = 0u;
+    std::cin >> lineNumber;
+
+    auto symbolNumber = lineNumber * (sizeOfStrings);
+
+    std::string string;
+    file.seekg(symbolNumber, std::ios::beg);
+    std::getline(file, string);
+    std::cout << string << std::endl;
+
+    return 0;
+}
