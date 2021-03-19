5c030c6d5cc6a5c39123de5a0af59efff64898dc
m-kichik <kichik.mg@phystech.edu>
2021-03-20 00:23:22 +0300

    deleted spaces

--- HW_6/1st task _ DELETING COMMENTS/DELETING_COMMENTS_V2.cpp
+++ HW_6/1st task _ DELETING COMMENTS/DELETING_COMMENTS_V2.cpp
@@ -5,10 +5,10 @@ int main() {
     std::fstream testIn ("../HW_6/1st task _ DELETING COMMENTS/test.txt", std::ios::in);
     std::ofstream testOut ("../HW_6/1st task _ DELETING COMMENTS/result.txt");
 
-    std::string code(
-            (std::istreambuf_iterator <char> (testIn)),
-            std::istreambuf_iterator <char> ()
-            );
+    std::string code{
+            (std::istreambuf_iterator<char>(testIn)),
+            std::istreambuf_iterator<char>()
+    };
 
     for (auto it = std::begin(code); it != std::end(code); ++it) {
         if (*it == '/') {
