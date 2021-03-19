c6dc7ed6ec7bee707c3b1f53e0f8690517d7aae6
m-kichik <kichik.mg@phystech.edu>
2021-03-20 00:20:48 +0300

    hw 7 task 1 v1

---
+++ HW_7/1st task _ CALCULATE PI/CALCULATE_PI.cpp
@@ -0,0 +1,58 @@
+#include <chrono>
+#include <future>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include "TIMER.h"
+
+auto preCalculatePi(std::size_t numberOfPoints) {
+    std::uniform_real_distribution urd(-1., 1.);
+    std::mt19937_64 mte(std::chrono::system_clock::now().time_since_epoch().count());
+
+    auto circlePointCounter = 0u;
+    const auto R = 1.;
+
+    for (auto i = 0u; i < numberOfPoints; ++i) {
+        auto x = urd(mte);
+        auto y = urd(mte);
+        circlePointCounter += (x * x + y * y <= R) ? 1u : 0u;
+    }
+
+    return circlePointCounter;
+}
+
+auto calculatePi_parallel(std::size_t numberOfPoints, std::size_t numberOfThreads) {
+    std::vector <std::future<unsigned>> futures;
+    futures.reserve(numberOfThreads);
+    for (; std::size(futures) < numberOfThreads;
+        futures.push_back(std::async(std::launch::async, preCalculatePi, numberOfPoints)));
+    auto result = 0u;
+    std::for_each(std::begin(futures), std::end(futures), [&result](auto& x){result += x.get();});
+
+    return 4 * static_cast<long double>(result) / numberOfPoints / numberOfThreads;
+}
+
+auto calculatePi_sequential(std::size_t numberOfPoints) {
+    return 4 * static_cast<long double>(preCalculatePi(numberOfPoints)) / (numberOfPoints);
+}
+
+int main() {
+    const auto numberOfPoints = 100000000u;
+    const auto numberOfThreads = 6u;
+
+    Timer tPar("parallel pi calculation");
+    auto pi_p = calculatePi_parallel(numberOfPoints, numberOfThreads);
+    tPar.printTime();
+    std::cout << "result : " << std::fixed << std::setprecision(10) << pi_p << std::endl;
+
+    Timer tSeq("sequential pi calculation");
+    auto pi_s = calculatePi_sequential(numberOfPoints * numberOfThreads);
+    tSeq.printTime();
+    std::cout << "result : " << pi_s << std::endl;
+
+    std::cout << "seq/par coefficient : " << static_cast<long double>(tSeq.getTime()) / tPar.getTime() << std::endl;
+
+    return 0;
+}
---
+++ HW_7/1st task _ CALCULATE PI/TIMER.h
@@ -0,0 +1,82 @@
+#ifndef CHRONO_TIMER_H
+#define CHRONO_TIMER_H
+
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <string>
+
+class Timer
+{
+public:
+    using clock_t = std::chrono::steady_clock;
+    using time_point_t = clock_t::time_point;
+    using interval_t = std::chrono::microseconds;
+
+public:
+    explicit Timer(const std::string &name) : m_name(name), m_begin(clock_t::now()) {}
+    Timer() : m_name("unknownTimer_"), m_begin(clock_t::now())
+    {
+        nameless_timer_counter ++;
+        m_name += std::to_string(nameless_timer_counter);
+    }
+
+public:
+    void stop()
+    {
+        m_full_time += std::chrono::duration_cast
+                < interval_t > (clock_t::now() - m_begin);
+        m_is_stopped = true;
+    }
+
+    void printTime()
+    {
+        if(!m_is_stopped)
+            stop();
+
+        std::cout << m_name <<  ", microseconds: " << m_full_time.count() << std::endl;
+    }
+
+    void goOn()
+    {
+        m_begin = clock_t::now();
+        m_is_stopped = false;
+    }
+
+    std::string getName()
+    {
+        return m_name;
+    }
+
+    auto getTime()
+    {
+        if(!m_is_stopped)
+            stop();
+
+        return m_full_time.count();
+    }
+
+public:
+    ~Timer() noexcept
+    {
+//        try
+//        {
+//            if(! m_is_stopped)
+//                stop();
+//            std::cout << m_name << ", milliseconds: " << m_full_time.count() << std::endl;
+//        }
+//        catch (std::exception& exception)
+//        {
+//            std::cerr << "timer crashed: " << exception.what() << std::endl;
+//        }
+    }
+
+private:
+    std::string m_name;
+    time_point_t m_begin;
+    interval_t m_full_time{0};
+    bool m_is_stopped = false;
+    inline static std::size_t nameless_timer_counter = 0u;
+};
+
+#endif //CHRONO_TIMER_H
