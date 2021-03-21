6db3daff7153db1cb3fbbcfa178573e707b316ba
m-kichik <kichik.mg@phystech.edu>
2021-03-22 00:10:51 +0300

    hw7 t3 v1.2

--- HW_7/3rd_task_PARALLEL_FOR_EACH.cpp
+++ HW_7/3rd_task_PARALLEL_FOR_EACH.cpp
@@ -5,20 +5,21 @@
 #include <vector>
 
 template <typename Iterator, typename Function>
-Function parallelForEach(Iterator firstIt, Iterator lastIt, Function func) {
+void parallelForEach(Iterator firstIt, Iterator lastIt, Function func) {
     const auto length = std::distance(firstIt, lastIt);
     const auto max_size = 25;
 
     if (length <= max_size) {
-        return std::for_each(firstIt, lastIt, func);
+        std::for_each(firstIt, lastIt, func);
+        return;
     } else {
         Iterator middleIt = firstIt;
         std::advance(middleIt, length / 2);
 
-        std::future <Function> firstRes = std::async(std::launch::async, parallelForEach<Iterator, Function>, firstIt, middleIt, func);
+        std::future <void> firstRes = std::async(std::launch::async, parallelForEach<Iterator, Function>, firstIt, middleIt, func);
         parallelForEach(middleIt, lastIt, func);
 
-        return firstRes.get();
+        return;
     }
 
 }
