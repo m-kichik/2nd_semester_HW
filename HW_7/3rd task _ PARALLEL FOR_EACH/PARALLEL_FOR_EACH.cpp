c7c4a66c229bc695151f804787f05622aaa568a5
m-kichik <kichik.mg@phystech.edu>
2021-03-21 19:57:11 +0300

    hw7 t3 v1

--- HW_7/3rd task _ PARALLEL FOR_EACH/PARALLEL_FOR_EACH.cpp
+++ HW_7/3rd task _ PARALLEL FOR_EACH/PARALLEL_FOR_EACH.cpp
@@ -1,4 +1,36 @@
-//
-// Created by Маргарита Кичик on 20.03.2021.
-//
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <numeric>
+#include <vector>
 
+template <typename Iterator, typename Function>
+Function parallelForEach(Iterator firstIt, Iterator lastIt, Function func) {
+    const auto length = std::distance(firstIt, lastIt);
+    const auto max_size = 25;
+
+    if (length <= max_size) {
+        return std::for_each(firstIt, lastIt, func);
+    } else {
+        Iterator middleIt = firstIt;
+        std::advance(middleIt, length / 2);
+
+        std::future <Function> firstRes = std::async(std::launch::async, parallelForEach<Iterator, Function>, firstIt, middleIt, func);
+        parallelForEach(middleIt, lastIt, func);
+
+        return firstRes.get();
+    }
+
+}
+
+int main() {
+    std::vector <double> container(1000);
+    std::iota(std::begin(container), std::end(container), 1.);
+    auto res = 0.;
+
+    parallelForEach(std::begin(container), std::end(container), [&res](auto x){res += x;});
+
+    std::cout << res; // res = 500500
+
+    return 0;
+}
