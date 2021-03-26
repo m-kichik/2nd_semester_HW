0f9efc913f8837a403f258e809825e01a69af8e6
m-kichik <kichik.mg@phystech.edu>
2021-03-26 22:05:39 +0300

    hw8 t1 v1, t2 v1.2

--- HW_8/1st_task_CALCULATE_PI_UPDATED.cpp
+++ HW_8/1st_task_CALCULATE_PI_UPDATED.cpp
@@ -2,43 +2,56 @@
 #include <future>
 #include <iomanip>
 #include <iostream>
+#include <mutex>
 #include <random>
 #include <vector>
 
-auto preCalculatePi(std::size_t numberOfPoints) {
-    std::uniform_real_distribution urd(-1., 1.);
-    std::mt19937_64 mte(std::chrono::system_clock::now().time_since_epoch().count());
-
-    auto circlePointCounter = 0u;
-    const auto R = 1.;
-
-    for (auto i = 0u; i < numberOfPoints; ++i) {
-        auto x = urd(mte);
-        auto y = urd(mte);
-        circlePointCounter += (x * x + y * y <= R) ? 1u : 0u;
+class parallelPiCalculator {
+private:
+    auto safeAddPoints(std::size_t points, std::size_t& accumulator) {
+        std::scoped_lock lock(m_mutex);
+        accumulator += points;
     }
 
-    return circlePointCounter;
-}
+    auto addCirclePoints(std::size_t numberOfPoints, std::size_t& pointAccumulator) {
+        std::uniform_real_distribution urd(-1., 1.);
+        std::mt19937_64 mte(std::chrono::system_clock::now().time_since_epoch().count());
 
-auto calculatePi_parallel(std::size_t numberOfPoints, std::size_t numberOfThreads) {
-    std::vector <std::future<unsigned>> futures;
-    futures.reserve(numberOfThreads);
+        auto circlePointCounter = 0u;
+        const auto R = 1.;
 
-    for (; std::size(futures) < numberOfThreads;
-           futures.push_back(std::async(std::launch::async, preCalculatePi, numberOfPoints)));
+        for (auto i = 0u; i < numberOfPoints; ++i) {
+            auto x = urd(mte);
+            auto y = urd(mte);
+            circlePointCounter += (x * x + y * y <= R) ? 1u : 0u;
+        }
 
-    auto result = 0u;
-    std::for_each(std::begin(futures), std::end(futures), [&result](auto& x){result += x.get();});
+        safeAddPoints(circlePointCounter, pointAccumulator);
+    }
 
-    return 4 * static_cast<long double>(result) / numberOfPoints / numberOfThreads;
-}
+public:
+    auto operator()(std::size_t numPoints, std::size_t numThreads) {
+        auto result = 0u;
+        std::vector <std::thread> threads(numThreads - 1);
+        for (auto it = 0u; it < std::size(threads); ++it) {
+            threads[it] = std::thread(&parallelPiCalculator::addCirclePoints, this, numPoints, std::ref(result));
+        }
+        addCirclePoints(numPoints, result);
+        for(auto& x : threads)
+            x.join();
+
+        return 4 * static_cast<long double>(result) / numPoints / numThreads;
+    }
+
+private:
+    std::mutex m_mutex;
+};
 
 int main() {
-    const auto numberOfPoints = 100000000u;
-    const auto numberOfThreads = 6u;
+    const auto numPoints = 100000000u;
+    const auto numThreads = (std::thread::hardware_concurrency()) ? std::thread::hardware_concurrency() : 1u;
 
-    std::cout << "result : " << std::fixed << std::setprecision(10) << calculatePi_parallel(numberOfPoints, numberOfThreads) << std::endl;
+    std::cout << "result : " << std::fixed << std::setprecision(10) << parallelPiCalculator()(numPoints, numThreads) << std::endl;
 
     return 0;
 }
